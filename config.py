be40a71b52b155e574e0adc65b0227f37eb98f58
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-07-17 10:53:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "25198711"))
+API_HASH = environ.get("API_HASH", "2a99a1375e26295626c04b4606f72752")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+ADMIN = int(environ.get("ADMIN", "1740287480"))          
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
